2dc6542a5f97184cc563236116b2ea4140a7abac
Jared Williams <jaredwilliamsmail@gmail.com>
2015-05-20 21:49:28 +0000

    form saving

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,28 @@ class MoviesController < ApplicationController
     
     def create
         @movie = Movie.new(movie_params)
+        
+        if @movie.save
+            redirect_to new_movie_path, notice: "Movie Saved!"
+        else
+            redirect_to new_movie_path, notice: "Error occurred"
+        end
+        
+    end
+    
+    def show
+    end
+    
+    def edit
+        @movie = Movie.find(movie_params)
+    end
+    
+    def update
+        if @movie.update(movie_params)
+            redirect_to @movie, notice: 'Your movie was successfully updated.'
+        else
+            render :edit
+        end
     end
     
     private
