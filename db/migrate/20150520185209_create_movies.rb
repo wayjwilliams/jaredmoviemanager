88cd6539f74dbcda5f13a4914ab4fc278d3998d5
Jared Williams <jaredwilliamsmail@gmail.com>
2015-05-20 18:54:27 +0000

    add movies table migration

---
+++ db/migrate/20150520185209_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :format
+      t.integer :length
+      t.integer :release_year
+      t.integer :rating
+      
+      t.timestamps null: false
+    end
+  end
+end
