ef12992706d932f11e6c955c85a80a919115daab
Jared Williams <jaredwilliamsmail@gmail.com>
2015-05-20 19:16:13 +0000

    add routes basic form new edit pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,13 @@ class MoviesController < ApplicationController
     def new
         @movie = Movie.new
     end
+    
+    def create
+        @movie = Movie.new(movie_params)
+    end
+    
+    private
+        def movie_params
+            params.require(:movie).permit(:title, :format, :length, :release_year, :rating)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :movies
   root 'pages#home'
 end
