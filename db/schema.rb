d447bd7b258fffb1ba067c83e0a4cb60ea9af276
Jared Williams <jaredwilliamsmail@gmail.com>
2015-06-05 06:48:30 +0000

    test commit

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150522193406) do
   add_index "movies", ["user_id"], name: "index_movies_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,5 +4,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_select "a[href=?]", movies_path
+    assert_select "a[href=?]", new_user_registration_path, count: 2
+    assert_select "a[href=?]", new_user_session_path, count: 2
+    assert_select "footer.navbar-fixed-bottom"
   end
 end
